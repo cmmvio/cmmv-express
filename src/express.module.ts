d2c5ac25c5850849955d5f2a14bad80097c8be40
Andre <andre@tos.world>
2025-02-10 22:01:13 -0300

    fix: remove express transpiler

--- src/express.module.ts
+++ src/express.module.ts
@@ -1,7 +1,2 @@
 import { Module } from '@cmmv/core';
-
-import { ExpressTranspiler } from './express.transpiler';
-
-export const ExpressModule = new Module('express', {
-    transpilers: [ExpressTranspiler],
-});
+export const ExpressModule = new Module('express', {});
